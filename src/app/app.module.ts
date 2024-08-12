d2a305fe09fab076035ee276cab74f6724273582
jofran21 <alvarezjofran123@gmail.com>
2024-08-12 14:28:46 -0500

    frontend1

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmpleadosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
